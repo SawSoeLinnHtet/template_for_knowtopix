f74684a6f10eb05a392f519cfa5dd6779c8afa20
Hazel <hazeldoom520@gmail.com>
2023-02-24 16:49:08 +0630

    footer slider and side bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          white: "#f5f5f5",
+          red: "#fff000",
+        },
+      },
+    },
   },
   plugins: [],
 }
